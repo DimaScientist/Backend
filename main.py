0f7a62a48b076377c96b5d309855014f129bc882
Andrei <ikosmov@gmail.com>
2020-04-21 18:31:48 +0400

    added getting lots from database: by id and just all

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import jsonify
+from bson.objectid import ObjectId
 import pymongo
 
 # везде выводить до 200 записи
@@ -6,21 +8,48 @@ import pymongo
 app = Flask(__name__)
 
 
+def get_connection():
+    return pymongo.MongoClient(
+        'mongodb://afanasiev_alexey:funny valentine did nothing wrong@140.82.36.93:27017/morning_wood')
+
+
+def get_db(connection):
+    return connection.get_database('morning_wood')
+
+
 def search_lots(id_lots):
-    pass
+    conn = get_connection()
+    db = get_db(conn)
+    collection = db.get_collection('lots')
+    res = []
+    if id_lots == '':
+        print("empty id")
+        for lot in collection.find():
+            temp = lot
+            temp['_id'] = str(temp['_id'])
+            res.append(temp)
+            print(temp)
+    else:
+        print("not empty id")
+        for lot in collection.find({'_id': ObjectId(id_lots)}):
+            temp = lot
+            temp['_id'] = str(temp['_id'])
+            res.append(temp)
+            print(temp)
+    return jsonify(res)
 
 
 def show_all_lots():
-    pass
+    return search_lots('')
 
 
 @app.route('/lots')
 @app.route('/lots/<id_lot>')
-def function_for_id_lots(id_lots=None):
+def function_for_id_lots(id_lot=None):
     # если пусто получить все данные о лотах
     # если id, то вывести все лоты
-    if id_lots:
-        return search_lots(id_lots)
+    if id_lot:
+        return search_lots(id_lot)
     else:
         return show_all_lots()
 
@@ -59,8 +88,9 @@ def get_data_login():
 
 @app.route('/mongo')
 def testMongoConnection():
-    conn = pymongo.MongoClient('mongodb://afanasiev_alexey:funny valentine did nothing wrong@140.82.36.93:27017/morning_wood')
-    name=conn.get_database().name
+    conn = pymongo.MongoClient(
+        'mongodb://afanasiev_alexey:funny valentine did nothing wrong@140.82.36.93:27017/morning_wood')
+    name = conn.get_database().name
     conn.close()
     return name
 
