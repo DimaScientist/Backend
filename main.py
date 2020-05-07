839c53ce02efb78e8daa43cd064ef4b4a730ec23
Andrei <ikosmov@gmail.com>
2020-05-07 20:08:41 +0400

    new auth check func

--- main.py
+++ main.py
@@ -15,6 +15,31 @@ secretKey = "makeAmericaGreatAgain"
 userTokenPassPattern = {'username': '', 'password_hash': '', 'time': 0}
 
 
+def auth(request):
+    print(request.headers)
+    auth = str(request.headers['Authorization'])
+    token = auth.split(' ')[1]
+    object = jwt.decode(token, secretKey, algorithms=['HS256'])
+    prevTimeS = object['time']
+    prevTime = int(prevTimeS)
+    currtime = get_current_time()
+    diff = currtime - prevTime
+    if diff > 2 * 60 * 60 * 1000:
+        return False
+    user = object['username']
+    password = object['password_hash']
+    users = get_db(get_connection())['users']
+    print(user)
+    print(password)
+    found = users.find({'username': user})
+    for user in found:
+        print(user)
+        print(user['password_hash'])
+        if user['password_hash'] == password:
+            return True
+    return False
+
+
 def get_connection():
     return pymongo.MongoClient(
         'mongodb://afanasiev_alexey:funny valentine did nothing wrong@140.82.36.93:27017/morning_wood')
@@ -57,43 +82,20 @@ def show_all_lots():
 @app.route('/lots')
 @app.route('/lots/<id_lot>')
 def function_for_id_lots(id_lot=None):
-    # если пусто получить все данные о лотах
-    # если id, то вывести все лоты
-    print(request.headers)
-    auth = str(request.headers['Authorization'])
-    token = auth.split(' ')[1]
-    object = jwt.decode(token, secretKey, algorithms=['HS256'])
-    prevTimeS = object['time']
-    prevTime = int(prevTimeS)
-    currtime = get_current_time()
-    diff = currtime - prevTime
-    if diff > 2 * 60 * 60 * 1000:
+    re = auth(request)
+    print(re)
+    if re:
+        if id_lot:
+            return search_lots(id_lot)
+        else:
+            return show_all_lots()
+    else:
         abort(401)
-        return
-    user = object['username']
-    password = object['password_hash']
-    users = get_db(get_connection())['users']
-    found = users.find({'username': user})
-    for user in found:
-        print(user)
-        print(user['password_hash'])
-        if user['password_hash'] == password:
-            if id_lot:
-                return search_lots(id_lot)
-            else:
-                return show_all_lots()
-    abort(401)
 
 
 @app.route('/')
 def hello():
     return 'This is a backend server for project Morning Wood'
-
-
-@app.route('/logout')
-def delete_data_autif():
-    # стирает данные о аутентификации
-    pass
 
 
 @app.route('/registration')
@@ -122,26 +124,12 @@ def register():
 
 @app.route('/login')
 def login():
+
     return 'asd'
 
 
-@app.route('/register')
-def store_password():
-    """
-    POST /
-      -- пароли уже провалидированы, не нужно сохранять сессию
-      request: json { login: string, password: string, paswordConfirm: string }
-      request: json { user: {login: string} }
-      -> 201
-    """
-    pass
-
-
-@app.route('/login')
-def get_data_login():
-    pass
-
-
 print(__name__)
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
+
+# app.run(debug=True)
